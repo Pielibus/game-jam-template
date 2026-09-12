eaa7a88ed08818d81f4dcc1f64869c1d1765bd6e
Pielibus <pielibus@gmail.com>
2026-09-13 00:05:23 +0200

    original commit

--- Assets/GameState/PlayerSpawningState.cs
+++ Assets/GameState/PlayerSpawningState.cs
@@ -45,10 +45,12 @@ public class PlayerSpawningState : StateNode
             playerCharacters[player] = newPlayer.gameObject;
             spawnedPlayers.Add(newPlayer);
             Debug.Log(playerCharacters + " " + playerCharacters[player]);
+            GameObject gobelet = newPlayer.transform.Find("Gobelet").gameObject;
+            gobelet.transform.parent = null;
+            gobelet.transform.position = Vector3.zero;
             currentSpawnIndex++;
         }
         
-        SetClients(playerCharacters);
         AllPlayers = spawnedPlayers;
         return spawnedPlayers;
     }
@@ -56,25 +58,6 @@ public class PlayerSpawningState : StateNode
     public override void Exit(bool asServer)
     {
         base.Exit(asServer);
-    }
-
-
-    [ObserversRpc(bufferLast:true)]
-    private void SetClients(Dictionary<PlayerID, GameObject> _playerCharacters)
-    {
-        foreach (var kvp in _playerCharacters)
-        {
-            Debug.Log(kvp);
-            Debug.Log(kvp.Value);
-            Debug.Log(kvp.Value.GetComponent<PlayerController>());
-            var newPlayer = kvp.Value.GetComponent<PlayerController>();
-            var playerName = "Player " + newPlayer.owner.Value;
-            GameObject gobelet = newPlayer.transform.Find("Gobelet").gameObject;
-            gobelet.transform.parent = null;
-            gobelet.name = "Gobelet " + newPlayer.owner.Value;
-            kvp.Value.name = playerName;
-
-        }
     }
 }
 
