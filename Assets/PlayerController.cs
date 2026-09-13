b7de046b231ce2f4e1d001000fec5974ca811f30
Pielibus <pielibus@gmail.com>
2026-09-13 18:59:05 +0200

    commit 4

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,25 +3,38 @@ using Unity.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using PurrNet;
+using PurrNet.StateMachine;
 
 public class PlayerController : NetworkBehaviour
 {
     [SerializeField] private GameObject[] AllDice;
-    [SerializeField] private GameObject Gobelet;
+    [SerializeField] public GameObject Gobelet;
+    [SerializeField] private RoundRunningState roundRunningState;
+    
     private NumberChecker numberChecker;
     void Awake()
     {
         numberChecker = GameObject.FindGameObjectWithTag("Ground").GetComponent<NumberChecker>();
+        roundRunningState = GameObject.FindAnyObjectByType<RoundRunningState>();
     }
     public void launchRolling()
     {
-        Debug.Log("Launching dices");
-        numberChecker.dicesCount.Clear();
-        foreach (var dice in AllDice)
-        {
-            dice.GetComponent<DiceRoll>().Roll();
-        }
+        if(roundRunningState.Running)
+            return;
+
+        // //Debug.Log("Launching dices");
+        // numberChecker.dicesCount.Clear();
+        // foreach (var dice in AllDice)
+        // {
+        //     dice.GetComponent<DiceRoll>().Roll();
+        // }
+        Gobelet.GetComponent<RollGobelet>().StartRoll();
         
+    }
+
+    public void StartRolling()
+    {
+        Gobelet.GetComponent<RollGobelet>().StartRoll();
     }
 
     public void CheckGobelet(InputAction.CallbackContext context)
